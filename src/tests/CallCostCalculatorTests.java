d18595fffcccee3f15f6da1d04314d9327ca66aa
bmh10 <bmh10@imperial.ac.uk>
2013-11-08 08:37:49 +0000

    Added CallCostCalculator tests.

--- src/tests/CallCostCalculatorTests.java
+++ src/tests/CallCostCalculatorTests.java
@@ -4,12 +4,11 @@ import static org.junit.Assert.*;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.HashMap;
+import java.util.ArrayList;
 
 import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.joda.time.DateTime;
-import org.joda.time.Period;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -20,13 +19,13 @@ import com.acmetelecom.CallCostCalculator;
 import com.acmetelecom.CallEnd;
 import com.acmetelecom.CallStart;
 import com.acmetelecom.DaytimePeakPeriod;
+import com.acmetelecom.DaytimePeakPeriod.DayPeriod;
 import com.acmetelecom.customer.Customer;
 import com.acmetelecom.customer.Tariff;
 import com.acmetelecom.customer.TariffLibrary;
 
 /**
  * Tests behaviour of CallCostCalculator in an isolated context.
- * @author BenLaptop
  */
 public class CallCostCalculatorTests {
 	
@@ -37,14 +36,15 @@ public class CallCostCalculatorTests {
 	@Rule
 	public ExpectedException exception = ExpectedException.none();
 	
-	Mockery context;
-	TariffLibrary mockTariffLibrary;
+	private Mockery context;
+	private TariffLibrary mockTariffLibrary;
+	private ArrayList<Tariff> tariffTypes;
 	
 	// Don't want to mock this as it is essential part of calculation.
-	DaytimePeakPeriod daytimePeakPeriod;
+	private DaytimePeakPeriod daytimePeakPeriod;
 	
 	// Instance across which tests are to be applied.
-	CallCostCalculator callCostCalculator;
+	private CallCostCalculator callCostCalculator;
 	
 	/**
 	 * Setup which is run before each unit test.
@@ -55,6 +55,10 @@ public class CallCostCalculatorTests {
 		daytimePeakPeriod = new DaytimePeakPeriod();
 		mockTariffLibrary = context.mock(TariffLibrary.class);
 		callCostCalculator = new CallCostCalculator(mockTariffLibrary, daytimePeakPeriod);
+		tariffTypes = new ArrayList<Tariff>();
+		for (Tariff t : Tariff.values()) {
+			tariffTypes.add(t);
+		}
 	}
 	
 	/**
@@ -67,110 +71,239 @@ public class CallCostCalculatorTests {
 	}
 	
 	/**
-	 * Tests that a call which starts and ends within same peak period is charged correctly.
+	 * Tests that a call which starts and ends within same peak period is charged correctly
+	 * for each tariff type.
 	 */
 	@Test
 	public void CallThatStartsAndEndsInSamePeakPeriodIsChargedCorrectly() {
-		// Setup.
-		int callLengthMins = 102;
-		Tariff tariff = Tariff.Standard;
-		DateTime startTime = new DateTime(2013, 11, 5, daytimePeakPeriod.PeakStart + 1, 0);
-		DateTime endTime = startTime.plusMinutes(callLengthMins);
-		Call call = this.setupCall(startTime, endTime);
-		Customer customer = this.setupCustomer(tariff);
-		
-		// Run.
-		BigDecimal result = callCostCalculator.calculateCallCost(customer, call);
-		
-		// Checks.
-		BigDecimal expectedCost = new BigDecimal(callLengthMins*60).multiply(tariff.peakRate());
-		expectedCost = expectedCost.setScale(0, RoundingMode.HALF_UP);
-		assertTrue(result.equals(expectedCost));
+		for (Tariff tariff : tariffTypes) {
+			// Setup.
+			int callLengthMins = 102;
+			DateTime startTime = new DateTime(2013, 11, 5, daytimePeakPeriod.PeakStart + 1, 0);
+			DateTime endTime = startTime.plusMinutes(callLengthMins);
+			Call call = this.setupCall(startTime, endTime);
+			Customer customer = this.setupCustomer(tariff);
+			
+			// Run.
+			BigDecimal result = callCostCalculator.calculateCallCost(customer, call);
+			
+			// Checks.
+			BigDecimal expectedCost = new BigDecimal(callLengthMins*60).multiply(tariff.peakRate());
+			expectedCost = expectedCost.setScale(0, RoundingMode.HALF_UP);
+			assertTrue(result.equals(expectedCost));
+		}
 	}
 	
 	/**
-	 * Tests that call which starts and ends in same off-peak period is charged correctly.
+	 * Tests that call which starts and ends in same off-peak period is charged correctly
+	 * for each tariff type.
 	 */
 	@Test
 	public void CallThatStartsAndEndsInSameOffPeakPeriodIsChargedCorrectly() {
-		// Setup.
-		int callLengthMins = 32;
-		Tariff tariff = Tariff.Standard;
-		DateTime startTime = new DateTime(2013, 11, 5, daytimePeakPeriod.PeakStart - 2, 54);
-		DateTime endTime = startTime.plusMinutes(callLengthMins);
-		Call call = this.setupCall(startTime, endTime);
-		Customer customer = this.setupCustomer(tariff);
-		
-		// Run.
-		BigDecimal result = callCostCalculator.calculateCallCost(customer, call);
-		
-		// Checks.
-		BigDecimal expectedCost = new BigDecimal(callLengthMins*60).multiply(tariff.offPeakRate());
-		expectedCost = expectedCost.setScale(0, RoundingMode.HALF_UP);
-		assertTrue(result.equals(expectedCost));
+		for (Tariff tariff : tariffTypes) {
+			// Setup.
+			int callLengthMins = 32;
+			DateTime startTime = new DateTime(2013, 11, 5, daytimePeakPeriod.PeakStart - 2, 54);
+			DateTime endTime = startTime.plusMinutes(callLengthMins);
+			Call call = this.setupCall(startTime, endTime);
+			Customer customer = this.setupCustomer(tariff);
+			
+			// Run.
+			BigDecimal result = callCostCalculator.calculateCallCost(customer, call);
+			
+			// Checks.
+			BigDecimal expectedCost = new BigDecimal(callLengthMins*60).multiply(tariff.offPeakRate());
+			expectedCost = expectedCost.setScale(0, RoundingMode.HALF_UP);
+			assertTrue(result.equals(expectedCost));
+		}
 	}
 	
 	/**
-	 * Tests that call which starts in off-peak period and ends in next peak period is charged correctly.
+	 * Tests that call which starts in off-peak period and ends in next peak period is charged correctly
+	 * for each tariff type.
 	 */
 	@Test
 	public void CallThatStartsInOffPeakPeriodAndEndsInPeakPeriodOnSameDayIsChargedCorrectly() {
-		// Setup.
-		int offPeakTime = 12;
-		int peakTime = 14;
-		Tariff tariff = Tariff.Standard;
-		DateTime startTime = new DateTime(2013, 11, 5, daytimePeakPeriod.PeakStart - 1, 60 - offPeakTime);
-		DateTime endTime = startTime.plusMinutes(offPeakTime + peakTime);
-		Call call = this.setupCall(startTime, endTime);
-		Customer customer = this.setupCustomer(tariff);
-		
-		// Run.
-		BigDecimal result = callCostCalculator.calculateCallCost(customer, call);
-		
-		// Checks.
-		BigDecimal expectedOffPeakCost = new BigDecimal(offPeakTime*60).multiply(tariff.offPeakRate());
-		BigDecimal expectedTotalCost = expectedOffPeakCost.add(new BigDecimal(peakTime*60).multiply(tariff.peakRate()));
-		expectedTotalCost = expectedTotalCost.setScale(0, RoundingMode.HALF_UP);
-		assertTrue(result.equals(expectedTotalCost));
+		for (Tariff tariff : tariffTypes) {
+			// Setup.
+			int offPeakTime = 12;
+			int peakTime = 14;
+			DateTime startTime = new DateTime(2013, 11, 5, daytimePeakPeriod.PeakStart - 1, 60 - offPeakTime);
+			DateTime endTime = startTime.plusMinutes(offPeakTime + peakTime);
+			Call call = this.setupCall(startTime, endTime);
+			Customer customer = this.setupCustomer(tariff);
+			
+			// Run.
+			BigDecimal result = callCostCalculator.calculateCallCost(customer, call);
+			
+			// Checks.
+			BigDecimal expectedOffPeakCost = new BigDecimal(offPeakTime*60).multiply(tariff.offPeakRate());
+			BigDecimal expectedTotalCost = expectedOffPeakCost.add(new BigDecimal(peakTime*60).multiply(tariff.peakRate()));
+			expectedTotalCost = expectedTotalCost.setScale(0, RoundingMode.HALF_UP);
+			assertTrue(result.equals(expectedTotalCost));
+		}
 	}
 	
 	/**
-	 * Tests that call which starts in peak period and ends in next off-peak period is charged correctly.
+	 * Tests that call which starts in peak period and ends in next off-peak period is charged correctly
+	 * for each tariff type.
 	 */
 	@Test
 	public void CallThatStartsInPeakPeriodAndEndsInOffPeakPeriodOnSameDayIsChargedCorrectly() {
-		// Setup.
-		int peakTime = 32;
-		int offPeakTime = 23;
-		Tariff tariff = Tariff.Standard;
-		DateTime startTime = new DateTime(2013, 11, 5, daytimePeakPeriod.PeakEnd - 1, 60 - peakTime);
-		DateTime endTime = startTime.plusMinutes(offPeakTime + peakTime);
-		Call call = this.setupCall(startTime, endTime);
-		Customer customer = this.setupCustomer(tariff);
-		
-		// Run.
-		BigDecimal result = callCostCalculator.calculateCallCost(customer, call);
-		
-		// Checks.
-		BigDecimal expectedOffPeakCost = new BigDecimal(offPeakTime*60).multiply(tariff.offPeakRate());
-		BigDecimal expectedTotalCost = expectedOffPeakCost.add(new BigDecimal(peakTime*60).multiply(tariff.peakRate()));
-		expectedTotalCost = expectedTotalCost.setScale(0, RoundingMode.HALF_UP);
-		assertTrue(result.equals(expectedTotalCost));
+		for (Tariff tariff : tariffTypes) {
+			// Setup.
+			int peakTime = 32;
+			int offPeakTime = 23;
+			DateTime startTime = new DateTime(2013, 11, 5, daytimePeakPeriod.PeakEnd - 1, 60 - peakTime);
+			DateTime endTime = startTime.plusMinutes(offPeakTime + peakTime);
+			Call call = this.setupCall(startTime, endTime);
+			Customer customer = this.setupCustomer(tariff);
+			
+			// Run.
+			BigDecimal result = callCostCalculator.calculateCallCost(customer, call);
+			
+			// Checks.
+			BigDecimal expectedOffPeakCost = new BigDecimal(offPeakTime*60).multiply(tariff.offPeakRate());
+			BigDecimal expectedTotalCost = expectedOffPeakCost.add(new BigDecimal(peakTime*60).multiply(tariff.peakRate()));
+			expectedTotalCost = expectedTotalCost.setScale(0, RoundingMode.HALF_UP);
+			assertTrue(result.equals(expectedTotalCost));
+		}
 	}
 	
-	// TODO: Multiple tests for calls which span multiple days and periods.
+	/**
+	 * Tests that a call that starts in pre-peak period and finished in post-peak period the next day
+	 * is charged correctly for each tariff type.
+	 */
+	@Test
+	public void CallThatStartsInPrePeakPeriodAndEndsInPostPeakPeriodOfNextDayIsChargedCorrectly() {
+		for (Tariff tariff : tariffTypes) {
+			// Setup.
+			int initialPrePeakTime = 45;
+			int finalPostPeakTime = 15;
+			DateTime startTime = new DateTime(2013, 11, 5, daytimePeakPeriod.PeakStart - 1, 60 - initialPrePeakTime);
+			DateTime endTime = new DateTime(2013, 11, 6, daytimePeakPeriod.PeakEnd, finalPostPeakTime);
+			Call call = this.setupCall(startTime, endTime);
+			Customer customer = this.setupCustomer(tariff);
+			
+			// Run.
+			BigDecimal result = callCostCalculator.calculateCallCost(customer, call);
+			
+			// Checks.
+			int preDuration = daytimePeakPeriod.getPeriodDurationSeconds(DayPeriod.PrePeak);
+			int peakDuration = daytimePeakPeriod.getPeriodDurationSeconds(DayPeriod.Peak);
+			int postDuration = daytimePeakPeriod.getPeriodDurationSeconds(DayPeriod.PostPeak);
+			
+			BigDecimal expectedOffPeakCost = 
+					new BigDecimal((initialPrePeakTime + finalPostPeakTime)*60 + postDuration + preDuration).multiply(tariff.offPeakRate());
+			BigDecimal expectedPeakCost = new BigDecimal(2*peakDuration).multiply(tariff.peakRate());
+			BigDecimal expectedTotalCost = expectedOffPeakCost.add(expectedPeakCost);
+			expectedTotalCost = expectedTotalCost.setScale(0, RoundingMode.HALF_UP);
+			assertTrue(result.equals(expectedTotalCost));
+		}
+	}
 	
-	// TODO: Test for call that goes over end of year into new year.
+	/**
+	 * Tests that a call that lasts for a week is charged correctly for each tariff type.
+	 */
+	@Test
+	public void CallThatLastsForAWeekIsChargedCorrectly() {
+		for (Tariff tariff : tariffTypes) {
+			// Setup.
+			int callTimeDays = 7;
+			DateTime startTime = new DateTime(2013, 11, 5, daytimePeakPeriod.PeakStart, 0);
+			DateTime endTime = startTime.plusDays(callTimeDays);
+			Call call = this.setupCall(startTime, endTime);
+			Customer customer = this.setupCustomer(tariff);
+			
+			// Run.
+			BigDecimal result = callCostCalculator.calculateCallCost(customer, call);
+			
+			// Checks.
+			int preDuration = daytimePeakPeriod.getPeriodDurationSeconds(DayPeriod.PrePeak);
+			int peakDuration = daytimePeakPeriod.getPeriodDurationSeconds(DayPeriod.Peak);
+			int postDuration = daytimePeakPeriod.getPeriodDurationSeconds(DayPeriod.PostPeak);
+			
+			BigDecimal expectedOffPeakCost = new BigDecimal((preDuration + postDuration)*callTimeDays).multiply(tariff.offPeakRate());
+			BigDecimal expectedPeakCost = new BigDecimal(peakDuration*callTimeDays).multiply(tariff.peakRate());
+			BigDecimal expectedTotalCost = expectedOffPeakCost.add(expectedPeakCost);
+			expectedTotalCost = expectedTotalCost.setScale(0, RoundingMode.HALF_UP);
+			assertTrue(result.equals(expectedTotalCost));
+		}
+	}
+	
+	/**
+	 * Tests that a call which goes into a new year is charged correctly for each tariff type.
+	 */
+	@Test
+	public void CallThatGoesIntoNewYearIsChargedCorrectly() {
+		for (Tariff tariff : tariffTypes) {
+			// Setup.
+			int callTimeDays = 2;
+			DateTime startTime = new DateTime(2013, 12, 31, daytimePeakPeriod.PeakStart, 0);
+			DateTime endTime = startTime.plusDays(callTimeDays);
+			Call call = this.setupCall(startTime, endTime);
+			Customer customer = this.setupCustomer(tariff);
+			
+			// Run.
+			BigDecimal result = callCostCalculator.calculateCallCost(customer, call);
+			
+			// Checks.
+			int preDuration = daytimePeakPeriod.getPeriodDurationSeconds(DayPeriod.PrePeak);
+			int peakDuration = daytimePeakPeriod.getPeriodDurationSeconds(DayPeriod.Peak);
+			int postDuration = daytimePeakPeriod.getPeriodDurationSeconds(DayPeriod.PostPeak);
+			
+			BigDecimal expectedOffPeakCost = new BigDecimal((preDuration + postDuration)*callTimeDays).multiply(tariff.offPeakRate());
+			BigDecimal expectedPeakCost = new BigDecimal(peakDuration*callTimeDays).multiply(tariff.peakRate());
+			BigDecimal expectedTotalCost = expectedOffPeakCost.add(expectedPeakCost);
+			expectedTotalCost = expectedTotalCost.setScale(0, RoundingMode.HALF_UP);
+			assertTrue(result.equals(expectedTotalCost));
+		}
+	}
+	
+	/**
+	 * Tests that call which lasts less than a minute but crosses from off-peak to peak period is charged correctly
+	 * for each tariff type.
+	 */
+	@Test
+	public void CallThatLastsAFewSecondsAndCrossesIntoPeakPeriodIsChargedCorrectly() {
+		for (Tariff tariff : tariffTypes) {
+			// Setup.
+			int offPeakSeconds = 13;
+			int peakSeconds = 17;
+			DateTime startTime = new DateTime(2013, 11, 5, daytimePeakPeriod.PeakStart - 1, 59, 60 - offPeakSeconds);
+			DateTime endTime = startTime.plusSeconds(offPeakSeconds + peakSeconds);
+			Call call = this.setupCall(startTime, endTime);
+			Customer customer = this.setupCustomer(tariff);
+			
+			// Run.
+			BigDecimal result = callCostCalculator.calculateCallCost(customer, call);
+			
+			// Checks.
+			BigDecimal expectedOffPeakCost = new BigDecimal(offPeakSeconds).multiply(tariff.offPeakRate());
+			BigDecimal expectedTotalCost = expectedOffPeakCost.add(new BigDecimal(peakSeconds).multiply(tariff.peakRate()));
+			expectedTotalCost = expectedTotalCost.setScale(0, RoundingMode.HALF_UP);
+			assertTrue(result.equals(expectedTotalCost));
+		}
+	}
 
-	// Sets up call with specified start and end time.
+	// --- Setup Methods --- //
+	
+	/**
+	 * Creates a call between the specified times using the dummy customers.
+	 * @param startTime The call start time.
+	 * @param endTime The call end time.
+	 * @return The call object.
+	 */
 	private Call setupCall(DateTime startTime, DateTime endTime) {
 		CallStart start = new CallStart(dummyCallerNumber, dummyCalleeNumber, startTime);
 		CallEnd end = new CallEnd(dummyCallerNumber, dummyCalleeNumber, endTime);
 		return new Call(start, end);
 	}
 	
-	// Sets up customer with specified tariff type and sets tariff library to return specified tariff
-	// type for this customer.
+	/** Sets up dummy customer with specified tariff type and sets tariff library to return specified tariff
+	 *  type for this customer.
+	 * @param tariffType The tariff type to set for the dummy customer.
+	 */
 	private Customer setupCustomer(final Tariff tariffType) {
 		final Customer customer = new Customer(dummyCustomerName, dummyCallerNumber, tariffType.toString());
 		context.checking(new Expectations() {{  
