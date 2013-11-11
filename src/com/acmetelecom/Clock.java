03e452f63b103797c85e83866644913c9d470935
bmh10 <bmh10@imperial.ac.uk>
2013-11-11 15:16:17 +0000

    Comments and added javadocs.

--- src/com/acmetelecom/Clock.java
+++ src/com/acmetelecom/Clock.java
@@ -2,11 +2,17 @@ package com.acmetelecom;
 
 import org.joda.time.DateTime;
 
+/**
+ * Basic implementation of a standard clock.
+ */
 public class Clock implements IClock {
 
+	/**
+	 * Gets the current time.
+	 * @return The current time as a DateTime object.
+	 */
 	@Override
 	public DateTime now() {
 		return DateTime.now();
 	}
-
 }
--- src/com/acmetelecom/IClock.java
+++ src/com/acmetelecom/IClock.java
@@ -2,7 +2,14 @@ package com.acmetelecom;
 
 import org.joda.time.DateTime;
 
+/**
+ * The clock interface which all clocks should implement.
+ */
 public interface IClock {
 
+	/**
+	 * Gets the current time.
+	 * @return The current time as a DateTime object.
+	 */
 	public DateTime now();
 }
